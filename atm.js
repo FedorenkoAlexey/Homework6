7061c10bbe4a5790ce80c1133946959fe351140d
Alexey Fedorenko <messer.ua@gmail.com>
2019-11-17 14:53:05 +0200

    input

--- atm.js
+++ atm.js
@@ -15,13 +15,15 @@ class Atm {
     } else return console.log("Error!");
   }
   showBalance() {
-    console.log(this.cashAtm);
+    // console.log(this.cashAtm);
     let but6 = document.getElementById("screen");
-    return (but6.innerHTML = this.sumAtm + "[" + this.cashAtm + "]");
+    return (but6.innerHTML =
+      "Balance ATM: " + this.sumAtm + " [" + this.cashAtm + "] ");
   }
   withdrawCash(card, sum) {
     let drawCash = 0;
     let banknotes = [];
+    let newArr = [];
     let newSum = sum;
     let sortArr = this.cashAtm
       .slice()
@@ -29,8 +31,8 @@ class Atm {
         return a - b;
       })
       .reverse();
-    if (card.sumCard < sum) return console.log("Not enought money in Card");
-    if (sum > this.sumAtm || sum < 0) return console.log("Not enought money");
+    if (card.sumCard < sum) return console.log("Not enough money in Card");
+    if (sum > this.sumAtm || sum < 0) return console.log("Not enough money");
     for (let i = 0; i <= sortArr.length; i++) {
       if (newSum >= sortArr[i]) {
         drawCash += sortArr[i];
@@ -38,35 +40,43 @@ class Atm {
         banknotes.push(sortArr[i]);
         sortArr.splice(i, 1, 0);
         if (drawCash == sum) {
-          this.cashAtm = sortArr;
           this.sumAtm -= drawCash;
           card.sumCard -= sum;
+          for (let i = 0; i <= sortArr.length; i++) {
+            if (sortArr[i] != 0) newArr.push(sortArr[i]);
+          }
+          this.cashAtm = newArr;
+
           return console.log("Good", drawCash, banknotes);
         }
       } else if (sum < sortArr[i]) continue;
     }
-    if (drawCash < sum)
-      return console.log("NO", sum, this.cashAtm, "draw", drawCash, sortArr);
+    if (drawCash < sum) return console.log("Choose another suum");
   }
 }
 
 class Card {
   constructor() {
-    this.sumCard = 0;
+    this.sumCard = 1000;
   }
 
   addMoney(sum) {
     if (sum > 0) this.sumCard += sum;
+
+    let sum1 = document.getElementById("atm-cash").value;
+    sum1 = parseInt(sum1);
+    document.getElementById("screen").innerHTML = sum1;
   }
   showBalance() {
-    return this.sumCard;
+    let but5 = document.getElementById("screen");
+    return (but5.innerHTML = "Balance Card: " + this.sumCard);
   }
 }
 
 let atm = new Atm();
 let card = new Card();
 
-card.addMoney(1000);
+// card.addMoney(1000);
 
 // atm.addMoney(950, [100, 50, 200, 500, 100]);
 // atm.showBalance(); // наличие денег в АТМ
